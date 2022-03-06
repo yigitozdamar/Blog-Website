a620bfa175b6c2a50d6a58bd736455b61322c495
yigitozdamar <yigitozdamar@hotmail.com>
2022-03-06 18:41:20 +0300

    post added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,10 +17,29 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
+});
+
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach((post) => {
+    const storedTitle = _.lowerCase(post.title);
+    if (requestedTitle === storedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
 });
 
 app.get("/contact", function (req, res) {
@@ -34,9 +54,14 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function (res, req) {
-  let composeTitle = res.body.composeText;
-  console.log(composeTitle);
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.composeTitle,
+    content: req.body.composePost,
+  };
+
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
